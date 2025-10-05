dd80a8358f7b978d98383cc53b2e20ac424cce62
ND Co., Ltd <nd.co.ltd.10@gmail.com>
2025-10-05 07:58:50 +0700

    Create data.js

---
+++ share/data.js
@@ -0,0 +1,18 @@
+const data = {
+    "Trang Chủ": "https://nd-site.github.io",
+    "BTVN-EN-01": "https://nd-site.github.io/video/btvn-en-01",
+    "BTVN-EN-02": "https://nd-site.github.io/video/btvn-en-02",
+    "BTVN-EN-03": "https://nd-site.github.io/video/btvn-en-03",
+    "BTVN-EN-04": "https://nd-site.github.io/video/btvn-en-04",
+    "Lạc Ngoài Không Gian": "https://www.rophim.me/xem-phim/lac-ngoai-khong-gian.Gam4bgGk",
+    "Losy In Space": "https://www.rophim.me/xem-phim/lac-ngoai-khong-gian.Gam4bgGk",
+    "Canva - Sunshine 01": "https://www.canva.com/design/DAGvetfI_es/uqV1o3aT6huZrg-dEfXuAg/edit?utm_content=DAGvetfI_es&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton",
+    "Video Khai Trương Sunshine 01": "https://nd-site.github.io/video/khai-truong-sunshine-01",
+    "Video Khai Trương Sunshine 02": "https://nd-site.github.io/video/khai-truong-sunshine-02",
+    "Nhập code Play Together VNG": "https://giftcode.vnggames.com/vn/redeem/661",
+    "Bình Dân Học Vụ Số": "https://binhdanhocvuso.gov.vn",
+    "ChatGPT - Trend Tân Nhạc Thính Phòng 01": "https://chatgpt.com/share/68a84616-3f04-800f-aa6d-2d948bb52212",
+    "Kí Tự Đặc Biệt": "https://bietmaytinh.com/ki-tu-dac-biet",
+    "Khảo Sát Học Sinh - SUNSHINE 01 - edit": "https://docs.google.com/forms/d/1eosA5Fkvo2cmK2Wn2h4oUnJ3Vv9heduBfyXgEa8E3pE/edit",
+    "Khảo Sát Học Sinh - SUNSHINE 01": "https://forms.gle/Hk8T2rtZa9fgQTSc8"
+};
