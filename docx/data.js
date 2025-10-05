cd0f6bab15a51dabcdfee6991a5d077cdeae1548
ND Co., Ltd <nd.co.ltd.10@gmail.com>
2025-10-05 08:07:10 +0700

    Create data.js

---
+++ docx/data.js
@@ -0,0 +1,7 @@
+const data = {
+    "ls10tx1": "https://nd-site.github.io/docx/ls10tx1",
+    "kttx1ls10-1": "https://nd-site.github.io/docx/kttx1ls10-1",
+    "kttx1ls10-2": "https://nd-site.github.io/docx/kttx1ls10-2",
+    "kttx1ls10-2mini": "https://nd-site.github.io/docx/kttx1ls10-2mini",
+    "kttx1ls10-3": "https://nd-site.github.io/docx/kttx1ls10-3"
+};
