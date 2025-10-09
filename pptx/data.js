134a6b6ea324aab7111e96225a15ee0213612222
ND Co., Ltd <nd.co.ltd.10@gmail.com>
2025-10-09 22:53:21 +0700

    Update data.js

--- pptx/data.js
+++ pptx/data.js
@@ -1,3 +1,3 @@
 const data = {
-    "pptx-robocon-01": "https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fnd-site%2Fnd-site.github.io%2Frefs%2Fheads%2Fmain%2Fpptx%2Ffile%2FRobocon%2520Error%2520404%2520Power%252001.pptx&wdOrigin=BROWSELINK",
+    "pptx-robocon-01": "https://nd-site.github.io/pptx/pptx-robocon-01",
 };
