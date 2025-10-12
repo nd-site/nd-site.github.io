d4dd6d8e884fc14723052ad3616f3fa5b10e2acc
ND Co., Ltd <nd.co.ltd.10@gmail.com>
2025-10-12 18:54:47 +0700

    Update data.js

--- docx/data.js
+++ docx/data.js
@@ -3,6 +3,6 @@ const data = {
     "kttx1ls10-1": "https://nd-site.github.io/docx/kttx1ls10-1",
     "kttx1ls10-2": "https://nd-site.github.io/docx/kttx1ls10-2",
     "kttx1ls10-2mini": "https://nd-site.github.io/docx/kttx1ls10-2mini",
-    "kttx1ls10-3": "https://nd-site.github.io/docx/kttx1ls10-3"
+    "kttx1ls10-3": "https://nd-site.github.io/docx/kttx1ls10-3",
     "banh-xe-orc-k3": "https://nd-site.github.io/docx/banh-xe-orc-k3",
 };
